9f8bdc48b3bb9f49ec3df9434d83142b85d8c89a
felipinhobgtom <felipinhobgouveia@gmail.com>
2025-01-20 16:40:18 -0300

    fix: fixed the form structure

--- src/app/components/form-component/form-component.component.ts
+++ src/app/components/form-component/form-component.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { PrimaryButtonComponent } from "../primary-button/primary-button.component";
 
 @Component({
   selector: 'app-form-component',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   templateUrl: './form-component.component.html',
   styleUrl: './form-component.component.scss'
 })
 export class FormComponentComponent {
-
+  onSubmit(): void {}
 }
