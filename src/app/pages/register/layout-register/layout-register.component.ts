f0617bd47e5732d5517691707561558222e44a65
felipinhobgtom <felipinhobgouveia@gmail.com>
2025-01-20 16:39:38 -0300

    feat: finished the register layout

--- src/app/pages/register/layout-register/layout-register.component.ts
+++ src/app/pages/register/layout-register/layout-register.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../../components/header/header.component";
-import { FooterComponent } from "../../../components/footer/footer.component";
-import { MainComponent } from "../../../components/main/main.component";
+import { HeaderComponent } from '../../../components/header/header.component';
+import { FooterComponent } from '../../../components/footer/footer.component';
+import { MainComponent } from '../../../components/main/main.component';
+import { SectionFormComponent } from '../../../components/section-form/section-form.component';
+import { InputComponentComponent } from '../../../components/input-component/input-component.component';
+import { FormComponentComponent } from '../../../components/form-component/form-component.component';
 
 @Component({
   selector: 'app-layout-register',
-  imports: [HeaderComponent, FooterComponent, MainComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    SectionFormComponent,
+    InputComponentComponent,
+    FormComponentComponent,
+  ],
   templateUrl: './layout-register.component.html',
-  styleUrl: './layout-register.component.scss'
+  styleUrl: './layout-register.component.scss',
 })
 export class LayoutRegisterComponent {
-
+  onSubmit(): void {
+    
+  }
 }
